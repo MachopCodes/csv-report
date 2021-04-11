248de412403a12b01f64f64285284e9626cce34d
machopcodes <william.andreae@uconn.edu>
2021-04-11 19:58:27 -0400

    completed formula

--- pages/api/csvconvert.js
+++ pages/api/csvconvert.js
@@ -1,22 +1,13 @@
+import { timeStamp } from 'console';
+
 const fs = require('fs')
 const util = require('util')
 
-// const object = {
-    // students: [
-        // studentObject
-        // id: 1
-        // name: A
-        // totalAvg: 90
-        // courses: [
-            // courseObject
-            // id: 1
-            // name: bio
-            // teacher: buckley
-            // courseavg: 90
-        // ]
-    // ]
-// }
 export default (req, res) => {
+    // Map Students: [ [0]id, [1]name ] then map Marks: [ [0]test_id, [1]student_id, [2]mark ] (store the mark)
+    // If student_id on the mark matches the student, map Tests: [ [0]test_id, [1] course_id, [2] weight ]
+    // if the test_id matches the test on the grade, map Courses: [ [0]course_id, [1]name, [2]teacher ]
+    // check if courses[] includes this course yet
     const readFile = util.promisify(fs.readFile)
     Promise.all([
         readFile('./public/marks.csv'), 
@@ -25,65 +16,39 @@ export default (req, res) => {
         readFile('./public/students.csv')
     ])
         .then((data) => {
-            const [marks, tests, courseObj, students] = data
-            const markArray = marks.toString().split('\n').map(m => m.replace('\r', ''))
-            const testArray = tests.toString().split('\n')
-            const courseArray = courseObj.toString().split('\n')
-            const studentArray = students.toString().split('\n')
-            const object = {
-                students: []
-            }
-            studentArray.shift()
-            studentArray.map(student => {
-                const s = student.split(',')
-                console.log('inside student', student[0])
-                const courses = []
-                let totalAverage = 0
-                markArray.map(mark => {
-                    // 0 test_id, 1 student_id, 2 mark
-                    // first find the student who got this grade
-                    // if mark student.id matches the student student.id
-                    // map through the test array
-                    const m = mark.split(',')
-                    if (m[1] === s[0]) testArray.map(test => {
-                            // 0 id, 1 course_id, 2 weight
-                            // next find the test that matches this test
-                            // if the mark test.id matches the test test.id
-                            // find the courses this student takes
-                            const t = test.split(',')
-                            if (m[0] === t[0]) {
-                                courseArray.map(course => {
-                                    // 0 id, 1 name, 2 teacher
-                                    // if the test course.id matches the course course.id
-                                    const c = course.split(',')
-                                    if (t[1] === c[0]) {
-                                        // if the object doesnt exist yet
-                                        // if (courses.includes(something)) 
-                                        courses.push({
-                                            id: c[0],
-                                            name: c[1],
-                                            teacher: c[2],
-                                            courseavg: m[2]*t[2]/100
-                                        })
-                                        // id: 1
-                                        // name: bio
-                                        // teacher: buckley
-                                        // courseavg: 90
-                                    }
-                                })
-
-                            }  
+            let split = (s) => s.toString().split('\n')
+            const [marks, tests, courses, students] = data,
+            markdata = split(marks).map(m => m.replace('\r', '')),
+            testdata = split(tests), coursedata = split(courses), studentdata = split(students),
+            object = { students: [] }
+            markdata.shift(); testdata.shift(); coursedata.shift(); studentdata.shift()
+            split = (string) => string.split(',')
+            studentdata.map(student => {
+                const sCourses = []
+                markdata.map(mark => {
+                    split(mark)[1] === split(student)[0] && testdata.map(test => {
+                        split(test)[0] === split(mark)[0] && coursedata.map(course => {
+                            const courseIndex = sCourses.findIndex(c => { return c.id === course[0] })
+                            if (split(course)[0] === split(test)[1]) {
+                                const wMark = Math.round(split(mark)[2] * split(test)[2] / 100)
+                                courseIndex === -1 
+                                    ? sCourses.push({
+                                        id: split(course)[0],
+                                        name: split(course)[1],
+                                        teacher: split(course)[2],
+                                        courseAverage: wMark
+                                    })
+                                    : sCourses[courseIndex].courseAverage += wMark
+                                }
+                            }) 
                         })
                     })
-                    object.students.push({ id: s[0], name: s[1], totalAverage })
-                })
-                console.log('object', object)
-            // markArray.map(m => {
-            //     const mark = m.split(',')
-            //     const studentMarks = mark.filter(m => m[1] === student[0])
-            //     console.log(studentMarks)
-            // })
-            return res.status(200).json(data).toString()
-        })  
+                    const sum = sCourses.reduce((r,a) => { return r + a.courseAverage }, 0)
+                    const totalAverage = (sum / sCourses.length).toFixed(2)
+                object.students.push({ id: split(student)[0], name: split(student)[1], totalAverage, courses: sCourses })
+            })
+            return res.status(200).json(object)
+        })
+        .catch(err => console.log(err)) 
 };
   
