55889b2f193a24d212de3bc9fb29b372ebd89431
machopcodes <william.andreae@uconn.edu>
2021-04-11 23:36:50 -0400

    added formatting

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,38 @@ import axios from "axios"
 
 export default function Index() {
   const [marks, setMarks] = useState()
+  const [clicked, setClicked] = useState(false)
   const handleClick = async (e) => {
     e.preventDefault()
+    setClicked(!clicked)
     const res = await axios.get(`/api/csvconvert`)
     setMarks(res.data)
   }
     console.log('data: ', marks)
+    const mString = marks ? JSON.stringify(marks).replace("\r", "") : null
 
   return (
     <div>
-      <p>hello this is an app</p>
+      <h1>Report Card File Reader</h1>
+      <h3>This application will read files if they are in the public folder and correctly formatted</h3>
+      <h4>files required:</h4>
+      <ul>
+        <li>courses.csv</li>
+        <li>marks.csv</li>
+        <li>students.csv</li>
+        <li>tests.csv</li>
+      </ul>
+      <h5>Files need to be in public folder and named in that way to be read by this system</h5>
       <button onClick={handleClick}>press this to read csv files</button>
+      {clicked 
+      ? mString
+        ? <div>
+            <h2>Success! Check the console for the JSON object. </h2>
+            <p>{mString}</p>
+          </div>
+        : <p>Loading</p>
+      : <p>JSON Output:</p>
+      }
     </div>
   );
 }
